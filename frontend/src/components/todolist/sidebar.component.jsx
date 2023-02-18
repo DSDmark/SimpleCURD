afa01ba431c4c591252e65d6dfe5bcbab30fb3ff
DSDmark <sahup3296@gmail.com>
2023-02-19 01:39:19 +0630

    FIX THE REDUX STATES AND UPDATE SIDER

--- frontend/src/components/todolist/sidebar.component.jsx
+++ frontend/src/components/todolist/sidebar.component.jsx
@@ -1,43 +1,41 @@
-import { useContext,useState } from 'react'
-import {TodoContext} from "../../context/"
 import { Grid, Paper, Box } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import { useSelector } from 'react-redux'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  fontSize:"1rem",
-  fontWeight:800,
+  fontSize: "1rem",
+  fontWeight: 800,
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  elevation:24,
+  elevation: 24,
+  background: theme.palette.primary.main,
+  padding: theme.spacing(4),
+  color: "white",
 }))
 
-
 const Sidebar = () => {
-  const todo = useContext(TodoContext);
-  const [selectedTodo,setSelectedTodo] = useState(null)
-
-
-  const setTodo = (currentTodo)=>{
-    setSelectedTodo(currentTodo)
-  }
-  
-  return(
+  const selectedTodo = useSelector((state) => state.todo.currentTodo);
+  const { title, desc, published } = selectedTodo;
+  return (
     <>
       <Box>
-        <Grid container rowSpacing={1}>
-          {todo.map((e) => (
-            <Grid key={e.id} item xs={12}>
-              <Item elevation={4} sx={selectedTodo==e.id?{bgcolor:"primary.main",color:"white"}:""} onClick={()=>setTodo(e.id)}>{e.title}</Item>
-            </Grid>
-          ))}
+        <Grid container rowSpacing={2}>
+          <Grid item xs={12}>
+            <Item elevation={4}>{title}</Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item elevation={4}>{desc}</Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item elevation={4} sx={published ? { bgcolor: "red" } : { bgcolor: "green" }}>{published ? " 🔴 Not Active" : "🟢Active "}</Item>
+          </Grid>
         </Grid>
       </Box>
-
-      </>
-  ) 
+    </>
+  )
 }
 
 export default Sidebar
--- frontend/src/slices/todo.js
+++ frontend/src/slices/todo.js
@@ -1,10 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import TodoDataService from '../services/todo.service.js'
 
-const initialState = []
+const initialState = { todos: [], currentTodo: {} }
 
 export const createTodo = createAsyncThunk('createData', async ({ title, desc }) => {
   const res = await TodoDataService.create({ title, desc })
+  return res.data;
 })
 
 export const retrieveTodo = createAsyncThunk('getData', async () => {
@@ -15,17 +16,22 @@ export const retrieveTodo = createAsyncThunk('getData', async () => {
 export const todoSlices = createSlice({
   name: 'todo',
   initialState,
+  reducers: {
+    setCurrentTodo: (state, action) => {
+      state.currentTodo = { ...action.payload }
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase('create', (state, action) => {
       state.push(action.payload)
     }),
       builder.addMatcher(
         (action) => action.type === retrieveTodo.fulfilled.type,
-        (state, action) => {return action.payload}
+        (state, action) => { return { ...state, todos: action.payload } }
       )
   },
 })
 
-const { reducer } = todoSlices
+export const { setCurrentTodo } = todoSlices.actions;
+export const todoReducer = todoSlices.reducer;
 
-export default reducer
