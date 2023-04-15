edafce0e0856a4faffc99dc254eb262d29b58959
DSDmark <sahup3296@gmail.com>
2023-04-15 21:32:35 +0530

    update files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = import.meta.env.VITE_LOCAL_PORT || 8000
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000,
+    port: PORT,
   }
 })
 
