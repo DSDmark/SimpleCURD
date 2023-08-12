9146df9eef67a985e73d3c1685086819ce0fa5b3
sanskar sahu <sahup3296@gmail.com>
2023-08-12 14:37:03 +0530

    fix docker-compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,17 @@
-import * as dotenv from "dotenv"
-import express from "express"
-import cors from "cors"
-import bodyParser from "body-parser"
-import router from "./routes/todo.routes.js"
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import router from './routes/todo.routes.js'
 
-dotenv.config();
-const app = express();
+const app = express()
 
-const PORT = process.env.NODE_LOCAL_PORT || 4000;
-const ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
+const PORT = process.env.NODE_LOCAL_PORT || 4000
+const ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 
 var corsOptions = {
-  origin: ORIGIN
+  origin: ORIGIN,
 }
 
 // middlewares
@@ -19,13 +19,12 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// routes 
-app.use("/api", router)
+// routes
+app.use('/api', router)
 
 // db config
-import "./config/db.config.js"
+import './config/db.config.js'
 
 app.listen(PORT, () => {
   console.log(`server are listens port ${PORT}`)
 })
-
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const PORT = import.meta.env.VITE_LOCAL_PORT || 8000
+const PORT = import.meta.VITE_LOCAL_PORT || 8000
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: PORT,
-  }
+  },
 })
-
-
